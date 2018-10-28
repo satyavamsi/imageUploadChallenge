757554a6bfd7f434745c911cb15169bf8c148aac
satyavamsi <satyavamsi222.21@gmail.com>
2018-10-27 17:18:58 -0700

    added upload function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 
 
 import { Component} from '@angular/core';
-import { Http, ResponseContentType } from '@angular/http';
+import { Http, ResponseContentType, RequestOptions } from '@angular/http';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import 'rxjs/operators';
+import { HttpHeaders, HttpClient, HttpRequest } from '@angular/common/http';
+import { Headers } from '@angular/http';
+
 
 
 
@@ -15,8 +18,9 @@ import 'rxjs/operators';
 export class AppComponent  {
 
   imageData: any;
+  imageFile: any;
 
-  constructor(private http: Http, private sanitizer: DomSanitizer) {
+  constructor(private http: Http, private sanitizer: DomSanitizer, private http2: HttpClient) {
   }
 
 
@@ -32,11 +36,41 @@ export class AppComponent  {
         let blob = new Blob([res._body], {
           type: res.headers.get("Content-Type")
         });
-
+        this.imageFile=blob;
         let urlCreator = window.URL;
         this.imageData = this.sanitizer.bypassSecurityTrustUrl(
             urlCreator.createObjectURL(blob));
       });
+  }
+
+  uploadImage() {
+    const url = 'https://edystupload.s3.ap-south-1.amazonaws.com/';
+    const headers = new HttpHeaders({'content-type': 'multipart/form-data'});
+    const formData: FormData = new FormData(); 
+
+  
+    const body= {
+      'x-amz-date' : '20181027T094242Z',
+      'x-amz-algorithm': 'AWS4-HMAC-SHA256',
+      'key': 'tempuploads/test123',
+      'x-amz-signature': '73f5606614f7cd8c96f74ccaa49a1c7a14fc05a76d73d2b7a7e61f3cdd4aa465',
+      'policy': 'eyJjb25kaXRpb25zIjogW3siYnVja2V0IjogImVkeXN0dXBsb2FkIn0sIFsic3RhcnRzLXdpdGgiLCAiJGtleSIsICJ0ZW1wdXBsb2Fkcy8iXSwgeyJ4LWFtei1hbGdvcml0aG0iOiAiQVdTNC1ITUFDLVNIQTI1NiJ9LCB7IngtYW16LWNyZWRlbnRpYWwiOiAiQUtJQUlDRUFTM1RGUUtZSVAyU0EvMjAxODEwMjcvYXAtc291dGgtMS9zMy9hd3M0X3JlcXVlc3QifSwgeyJ4LWFtei1kYXRlIjogIjIwMTgxMDI3VDA5NDI0MloifV0sICJleHBpcmF0aW9uIjogIjIwMTgtMTAtMjlUMDk6NDI6NDJaIn0=',
+      'x-amz-credential': 'AKIAICEAS3TFQKYIP2SA/20181027/ap-south-1/s3/aws4_request',
+      'file': this.imageFile
+    }
+    for(let key in body){
+      formData.append(key, body[key]);
+    }
+
+    const req = new HttpRequest('POST',url, formData, {
+      reportProgress: true // for progress data
+    });
+
+
+    this.http2.request(req)
+              .subscribe((res) =>  {
+                  console.log(res);
+              });
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { HttpModule } from '@angular/http';
   imports: [
     BrowserModule,
     HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
